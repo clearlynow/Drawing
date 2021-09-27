9e538764ee57f820df7521bdebcdb988a9bf0828
clearlynow <59779189+clearlynow@users.noreply.github.com>
2021-09-27 15:03:07 -0600

    Initial Commit

---
+++ Shared/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Shared
+//
+//  Created by Alison Gorman on 9/27/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
